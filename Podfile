5621a6b43479d951be55f72e8a24b8a3ad80843a
Jacob Frick <jacobfrick@Jacobs-MacBook-Air.local>
2018-09-15 06:32:29 -0700

    Initial Commit
    Also creation of prototype cell for table view and swift files

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Tumblr_Clone' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Tumblr_Clone
+	pod 'AlamofireImage'
+  target 'Tumblr_CloneTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Tumblr_CloneUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Tumblr_Clone/imageCell.swift
@@ -0,0 +1,24 @@
+//
+//  imageCellTableViewCell.swift
+//  Tumblr_Clone
+//
+//  Created by Jacob Frick on 9/15/18.
+//  Copyright © 2018 Jacob Frick. All rights reserved.
+//
+
+import UIKit
+
+class imageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
